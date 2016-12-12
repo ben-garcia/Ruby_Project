6f30e9c901979dbe61abdb47e131663b1f7fb8be
Ben <7rubengarcia7@gmail.com>
2016-12-12 13:40:41 -0800

    added a few gems

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
 
   # This method allows params(title and id) of the articles to be modified.
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 
 end
--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -4,6 +4,10 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  # This has_attached_file method is part of the paperclip library. With that declaration, paperclip will understand that this model should accept a file 
+  # attachment and that there are fields to store information about that file which start with image_ in this model’s database table.
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     self.tags.collect do |tag|
