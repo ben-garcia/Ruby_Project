42c428c0ce0129e7d7b4b647a2a1d482c6657226
Ben <7rubengarcia7@gmail.com>
2016-12-12 10:58:39 -0800

    Tagging feature completed

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
 
   # This method allows params(title and id) of the articles to be modified.
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 end
--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -2,5 +2,19 @@ class Article < ApplicationRecord
   #We then want to declare the other side of the relationship
   #Now an article "has many" comments, and a comment "belongs to" an article. We have explained to Rails that these objects have a one-to-many relationship
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 
 end
